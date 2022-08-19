f094aa289951ad30f8b744c9b0682c41da7840d0
unknown <cogjava3123@iiht.tech>
2022-08-19 00:36:42 -0700

    Pending at authentication

--- src/main/java/com/token/demo/JwtDemoApplication.java
+++ src/main/java/com/token/demo/JwtDemoApplication.java
@@ -2,12 +2,18 @@ package com.token.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class JwtDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtDemoApplication.class, args);
+	}
+	
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
